b9ad397bbb1684b85a2667fe53947f51464f9b44
MySecoundLanguage <mmamunism@gmail.com>
2022-03-19 11:05:02 +0600

    Added basci

--- src/App.js
+++ src/App.js
@@ -1,12 +1,33 @@
-import logo from './logo.svg';
 
 
 function App() {
   return (
 
-          <>
-              <p>he</p>
-            </>
+        <>
+          <div className="vh-100 d-flex justify-content-center align-items-center bg-light bg-gradient">
+            <div className="vh-100 d-flex justify-content-center align-items-center">
+              <div>
+                <div className="d-flex justify-content-center">
+                  <button className="btn btn-success btn-lg btn-block">hipster</button>
+                </div>
+                <h1>This codebase is auto generated using <span className="bg-success text-white border border-danger">HYPER HELIOS</span> Scaffolding</h1>
+
+                <div>
+                   <h4 className="alert alert-danger">Important: A few info you must read</h4>
+
+                   <ul className="list-group">
+                      <li className="list-group-item">We have not added any stylesheet except bootstrap5</li>
+                      <li className="list-group-item">There are lots of blank scss file, please put your style in relavant stylesheet</li>
+                      <li className="list-group-item">Don't change anything in bootstrap folder</li>
+                      <li className="list-group-item">Every component should be re-usable</li>
+                      <li className="list-group-item">Write manageable CSS, otherwise you may go crazy !</li>
+                    </ul>
+                    <p className="alert alert-success">Happy coding...</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </>
 
   );
 }
